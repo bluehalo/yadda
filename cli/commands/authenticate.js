453432b46dd2253b71f720cada4ffed075b5c3da
Sumeet Shah <sshah@asymmetrik.com>
2019-01-02 16:59:44 -0500

    Grammar and spelling fixes

--- cli/commands/authenticate.js
+++ cli/commands/authenticate.js
@@ -7,11 +7,13 @@ const logger = config.logger;
 
 module.exports = function(program) {
 	program.command('authenticate')
-		.description('Obtain AWS session credentials using a user profile from  ~/.aws/credentials and saves them to ' +
-			'./.env.session. This file will be overwritten every time this command is run. If you do not have ' +
-			'a ~/.aws/credentials file, run the cli command \'aws configure\' and enter your AWS Access Key ID and ' +
-			'Secret Access Key at the prompts.\n\nIf you already have a ~/.aws/credentials file, you can add new user ' +
-			'profiles by appending them to the bottom of the file. Profiles should be in the following format:\n\n' +
+		.description('Authenticate')
+		.description('Authenticates CLI access to AWS resources for a named profile using an MFA token. When a valid ' +
+			'profile from  ~/.aws/credentials and the associated MFA Token are supplied, temporary session credentials ' +
+			'are saved to ./.env.session. The ./.env.session file will be overwritten every time this command is run with ' +
+			'valid credentials.\n\nIf you do not have a ~/.aws/credentials file, run the cli command \'aws configure\' ' +
+			'and enter your AWS Access Key ID and Secret Access Key at the prompts. If you already have a ~/.aws/credentials ' +
+			'file, you can add new named profiles by appending them to the bottom of the file. Profiles should be in the following format:\n\n' +
 			'[PROFILE_NAME]\naws_access_key_id=AWS_ACCESS_KEY_ID\naws_secret_access_key=AWS_SECRET_ACCESS_KEY\n\n' +
 			'WARNING: If you have set environment variables in your development environment containing AWS credentials, ' +
 			'they may interfere with the authentication process. It is recommended that you unset these before attempting ' +
@@ -23,17 +25,17 @@ module.exports = function(program) {
 			const runtimeOptions = this.opts();
 			let promptedOptions = {
 				properties: {
-					token: {
-						description: 'MFA Token',
-						pattern: /^[\d]{6}$/,
-						type: 'string',
-						required: true
-					},
 					profile: {
 						description: 'Profile from ~/.aws/credentials to use',
 						type: 'string',
 						required: true,
 						default: 'default'
+					},
+					token: {
+						description: 'MFA Token',
+						pattern: /^[\d]{6}$/,
+						type: 'string',
+						required: true
 					}
 				}
 			};
